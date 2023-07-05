ed6af44641d2e6cf00e9276491a94fd40ef90ee8
s-a-c-1-8 <Sac@8teen.in>
2023-07-05 18:24:01 +0530

    testing done

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const PORT = 4000;
 const path = require("path");
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-const  bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-app.use("/public",express.static(path.join(__dirname,"public")));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use("/public", express.static(path.join(__dirname, "public")));
+const { Configuration, OpenAIApi } = require('openai');
 
-server.listen(PORT,()=>{ console.log(`server started on ${PORT}`) })
+server.listen(PORT, () => { console.log(`server started on ${PORT} `) })
 
 app.use(cors());
 
@@ -34,4 +35,95 @@ app.use((req, res, next) => {
 
 app.get('/', function (req, res) {
   res.render("main.ejs");
+});
+
+// const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function getAnswer(prompt) {
+  try {
+    const completion = await openai.createCompletion({
+      engine: "text-davinci-003", // Specify the engine
+      prompt: prompt,
+      max_tokens: 100,
+      temperature: 0,
+      n: 1,
+      stop: "\n"
+    });
+
+    const answer = completion.data.choices[0].text.trim();
+    console.log("Answer:", answer);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+
+app.post('/get-data', async (req, res) => {
+  console.log(req.body);
+  
+  
+// Example usage
+// const prompt = "Who is Virat Kohli?";
+try {
+  const { messages } = {
+    "messages": [
+      { "role": "system", "content": "You are a helpful assistant." },
+      { "role": "system", "content": "Provide a short answer." },
+      { "role": "user", "content":`${req.body.prompt}` }
+    ]
+  };
+
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+    // max_tokens: 50,
+    temperature: 0.7
+  });
+
+  const response = completion.data.choices[0].message;
+  console.log(response.content);
+  let result = response.content
+  res.json({ result });
+} catch (error) {
+  console.error('Error:', error.message);
+  res.status(500).json({ error: 'Something went wrong' });
+}
+
+  // const prompt = req.body.prompt; // Assuming prompt is available in the request body
+  // const endpoint = 'https://api.openai.com/v1/engines/davinci/completions';
+  // const requestBody = {
+  //   prompt: prompt,
+    // max_tokens: 100,
+    // temperature: 0.7,
+    // n: 1,
+  //   // model: 'text-davinci-003', 
+  //   // stop: ['\n']
+  // };
+
+  // const requestOptions = {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+  //   },
+  //   body: JSON.stringify(requestBody)
+  // };
+
+  // try {
+  //   const response = await fetch(endpoint, requestOptions); // Assuming 'endpoint' is defined
+  //   const data = await response.json();
+  //   const answer = data.choices[0].text.trim();
+  //   console.log('Answer:', answer);
+  //   const regex = /[^\w\s]/gi;
+  //   const result = answer.replace(regex, '');
+  //   res.json({ result });
+
+  // } catch (error) {
+  //   console.error('Error:', error);
+  // }
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,113 +1,11 @@
 let recognition;
 let isRecording = false;
 var transcript
-
-
-// // ChatGPT API 
-// const apiKey = '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';
-// // const endpoint = 'https://api.openai.com/v1/engines/davinci/completions';
-// const { Configuration, OpenAIApi } = require('openai');
-
-// const configuration = new Configuration({
-//   apiKey: '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',
-// });
-// const openai = new OpenAIApi(configuration);
-
-const sendPrompt = async (prompt) => {
-//   const requestBody = {
-//     prompt: prompt,
-//     max_tokens: 100,
-//     temperature: 0.7,
-//     n: 1,
-//     model: 'text-davinci-003', 
-//     // stop: ['\n']
-//   };
-
-  // const requestOptions = {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Authorization': `Bearer ${apiKey}`
-  //   },
-  //   body: JSON.stringify(requestBody)
-  // };
-
-  try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      max_tokens: 100,
-      temperature: 1,
-    });
-    // const response = await fetch(endpoint, requestOptions);
-    const data = await response.json();
-    const answer = data.choices[0].text.trim();
-    console.log('Answer:', answer);
-    if(!answer){
-      textToSpeech('My name is sac')
-    }else{
-      textToSpeech(answer)
-    }
-    
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
-
-// Start Recording 
-const startRecording = () => {
-  recognition = new webkitSpeechRecognition();
-  recognition.continuous = true;
-  recognition.interimResults = true;
-
-  recognition.onresult = (event) => {
-    const result = event.results[event.results.length - 1];
-    transcript =result[0].transcript
-    console.log('Speech Recognition Result:', transcript);
-  };
-
-  recognition.onend = () => {
-    console.log('Speech Recognition Ended');
-    isRecording = false;
-  };
-
-  recognition.start();
-  isRecording = true;
-  console.log('Speech Recognition Started');
-};
-
-// Stop Recording
-const stopRecording = () => {
-  if (recognition && isRecording) {
-    recognition.stop();
-    isRecording = false;
-    console.log(transcript);
-    sendPrompt(transcript);
-  //   const finalResultPromise = new Promise((resolve) => {
-  //     recognition.onend = () => {
-  //       console.log('Speech Recognition Ended');
-  //       const finalResult = recognition.result;
-  //       console.log(finalResult);
-  //       resolve(finalResult);
-  //     };
-  //   });
-
-  //   finalResultPromise.then((finalResult) => {
-  //     console.log('Final Result:', finalResult);
-  //     // Perform further actions with the final result
-  //   });
-  }
-};
-
-document.getElementById('startButton').addEventListener('click', startRecording);
-document.getElementById('stopButton').addEventListener('click', stopRecording);
-
-
 // Send data to Speech
 const textarea = document.querySelector("textarea"),
 voiceList = document.querySelector("select"),
 speechBtn = document.querySelector("button");
-let synth = speechSynthesis,
+const synth = window.speechSynthesis;
 isSpeaking = true;
 
 voices();
@@ -123,6 +21,7 @@ function voices(){
 synth.addEventListener("voiceschanged", voices);
 
 function textToSpeech(text){
+  console.log('speaking');
     let utterance = new SpeechSynthesisUtterance(text);
     for(let voice of synth.getVoices()){
         if(voice.name === voiceList.value){
@@ -131,3 +30,88 @@ function textToSpeech(text){
     }
     synth.speak(utterance);
 }
+const MAX_SEGMENT_LENGTH = 200; // Maximum segment length in characters
+
+function textToSpeechSegments(text) {
+  const segments = [];
+  let currentIndex = 0;
+
+  while (currentIndex < text.length) {
+    const segment = text.substr(currentIndex, MAX_SEGMENT_LENGTH);
+    segments.push(segment);
+    currentIndex += MAX_SEGMENT_LENGTH;
+  }
+
+  segments.forEach((segment, index) => {
+    const delay = index * 500; // Delay between segments (adjust as needed)
+    setTimeout(() => {
+      textToSpeech(segment);
+    }, delay);
+  });
+}
+const sendPrompt = async (prompt) => {
+    let data = {
+    prompt:prompt
+  }
+  fetch('http://localhost:4000/get-data', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Process the response data
+      console.log(data.result);
+      let answer = data.result
+      
+      textToSpeechSegments(answer)
+    })
+    .catch(error => {
+      // Handle any errors
+    });
+};
+
+// Start Recording 
+const startRecording = () => {
+  // textToSpeech("Hello, this is a test.");
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+
+  recognition.onresult = (event) => {
+    const result = event.results[event.results.length - 1];
+    transcript =result[0].transcript
+    document.getElementById("voiceData").value = transcript
+    
+    console.log('Speech Recognition Result:', transcript);
+  };
+
+  recognition.onend = () => {
+    console.log('Speech Recognition Ended');
+    isRecording = false;
+    sendPrompt(transcript)
+  };
+
+  recognition.start();
+  isRecording = true;
+  console.log('Speech Recognition Started');
+};
+
+// Stop Recording
+const stopRecording = () => {
+  // let data = 'hello abhijeet how are you'
+  // textToSpeech(data)
+  if (recognition && isRecording) {
+    recognition.stop();
+    isRecording = false;
+    console.log(transcript);
+    sendPrompt(transcript);
+  }
+};
+
+document.getElementById('startButton').addEventListener('click', startRecording);
+document.getElementById('stopButton').addEventListener('click', stopRecording);
+
+
