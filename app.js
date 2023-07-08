7d9513d28ec99016d14d3436ca47a37ea6220070
s-a-c-1-8 <Sac@8teen.in>
2023-07-08 18:52:56 +0530

    final upadate

--- app.js
+++ app.js
@@ -5,23 +5,28 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = 4000;
 const path = require("path");
-const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
-const perf = require('execution-time')();
+require('dotenv').config();
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const voice = require('elevenlabs-node');
+const fs = require('fs');
+const labApiKey = process.env.ELEVEN_LAB_API_KEY; // Your API key from Eleven Labs
+const voiceID = '21m00Tcm4TlvDq8ikWAM';            // The ID of the voice you want to get
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.json({ extended: true, limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use("/public", express.static(path.join(__dirname, "public")));
-const { Configuration, OpenAIApi } = require('openai');
 
 server.listen(PORT, () => { console.log(`server started on ${PORT} `) })
 
 app.use(cors());
-
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -40,20 +45,8 @@ app.get('/', function (req, res) {
   res.render("main.ejs");
 });
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-const voice = require('elevenlabs-node');
-const fs = require('fs');
-
-const apiKey = process.env.ELEVEN_LAB_API_KEY; // Your API key from Eleven Labs
-const voiceID = '21m00Tcm4TlvDq8ikWAM';            // The ID of the voice you want to get
-const textInput = `Hey this on eis tetsing`
-
 io.on('connection', function (socket) {
   console.log("connceted");
-
 })
 
 app.post('/get-data', async (req, res) => {
@@ -62,7 +55,7 @@ app.post('/get-data', async (req, res) => {
     const { messages } = {
       "messages": [
         { "role": "system", "content": "Provide a short answer." },
-        { "role": "user", "content":`${req.body.prompt}` }
+        { "role": "user", "content": `${req.body.prompt}` }
       ]
     };
 
@@ -70,13 +63,13 @@ app.post('/get-data', async (req, res) => {
       model: 'gpt-3.5-turbo',
       messages: messages,
       // max_tokens: 50,
-      temperature: 0
+      temperature: 0.2
     });
 
     const response = completion.data.choices[0].message;
     console.log(response.content);
     let result = response.content
-    voice.textToSpeechStream(apiKey, voiceID, result,0.2,0.7).then(res => {
+    voice.textToSpeechStream(labApiKey, voiceID, result, 0.2, 0.7).then(res => {
       var fileName = ''
       var num = ''
       num = Date.now()
@@ -85,46 +78,13 @@ app.post('/get-data', async (req, res) => {
       res.pipe(writeStream);
       writeStream.on('finish', () => {
         console.log('Speech generated successfully.');
-        io.emit('play',num)
+        io.emit('play', num)
       });
     });
     // res.json({ result });
-    return
   } catch (error) {
     console.error('Error:', error.message);
     res.status(500).json({ error: 'Something went wrong' });
   }
 
-  // const prompt = req.body.prompt; // Assuming prompt is available in the request body
-  // const endpoint = 'https://api.openai.com/v1/engines/davinci/completions';
-  // const requestBody = {
-  //   prompt: prompt,
-  // max_tokens: 100,
-  // temperature: 0.7,
-  // n: 1,
-  //   // model: 'text-davinci-003', 
-  //   // stop: ['\n']
-  // };
-
-  // const requestOptions = {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-  //   },
-  //   body: JSON.stringify(requestBody)
-  // };
-
-  // try {
-  //   const response = await fetch(endpoint, requestOptions); // Assuming 'endpoint' is defined
-  //   const data = await response.json();
-  //   const answer = data.choices[0].text.trim();
-  //   console.log('Answer:', answer);
-  //   const regex = /[^\w\s]/gi;
-  //   const result = answer.replace(regex, '');
-  //   res.json({ result });
-
-  // } catch (error) {
-  //   console.error('Error:', error);
-  // }
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,39 @@
-let recognition;
+var recognition;
 let isRecording = false;
 var transcript
 var buttonId
 var audio
 var startTime;
 const socket = io();
+
+
+function playVid() {
+  
+}
+
 var audio = document.getElementById("myAudio");
 socket.on('play', (e) => {
+  document.getElementById("default").style.display = 'none'
+  document.getElementById("bg").style.display = 'block'
+  document.getElementById("third").style.display = 'none'
+  document.getElementById("four").style.display = 'block'
+  document.getElementById("voiceData").value = '';
   audio.src = ''
   audio.src = `../public/${e}.mp3`
   audio.play();
 })
 
+audio.onended = () => {
+  // Handle the "end" event here
+  // console.log("Audio playback ended");
+  location.reload()
+};
+
+// API endpoint
 // AZnzlk1XvdvUeBnXmlld  Domi  
 // 21m00Tcm4TlvDq8ikWAM Rachel
 // EXAVITQu4vr4xnSDxMaL Bells
 // MF3mGyEYCl7XYWbV9V6O Elli
-// ErXwobaYiN019PkySvjV Antoni
-// TxGEqnHWrfWFTfGW9XjX Josh
 
 const sendPrompt = async (prompt) => {
   let data = {
@@ -32,50 +48,138 @@ const sendPrompt = async (prompt) => {
   })
     .then(response => response.json())
     .then(data => {
-  
+      console.log(data.result);
     })
     .catch(error => {
-      // Handle any errors
     });
 };
 
 // Start Recording 
 const startRecording = () => {
-  audio.src = ''
-  document.getElementById("voiceData").value = ''
+
+  audio.src = '';
+  document.getElementById("voiceData").value = '';
   startTime = Date.now();
   console.log(startTime);
+
+  let isListeningRoboTriggered = false;
+
+  const startListeningRobo = () => {
+    document.getElementById("default").style.display = 'none'
+    document.getElementById("bg").style.display = 'block'
+    document.getElementById("first").style.display = 'none'
+    document.getElementById("second").style.display = 'block'
+    isRecording = true;
+    recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onresult = (event) => {
+      const result = event.results[event.results.length - 1];
+      transcript = result[0].transcript;
+
+      document.getElementById("voiceData").value = transcript;
+      console.log('Speech Recognition Result:', transcript);
+      // Additional logic or processing for the recognized speech can be added here.
+    };
+
+    // recognition.onend = () => {
+    //   console.log('Speech Recognition Ended');
+    //   isRecording = false;
+    //   sendPrompt(transcript)
+    // };
+
+    recognition.start();
+    
+    console.log('Speech Recognition Started');
+  };
+
+  const triggerListeningRobo = () => {
+    if (!isListeningRoboTriggered) {
+      isListeningRoboTriggered = true;
+      console.log('Listening Robo Triggered');
+      startListeningRobo();
+      
+      // Additional actions or animations can be performed here to indicate that the robot is listening.
+    }
+  };
+
   recognition = new webkitSpeechRecognition();
   recognition.continuous = true;
   recognition.interimResults = true;
 
   recognition.onresult = (event) => {
     const result = event.results[event.results.length - 1];
-    transcript = result[0].transcript
-    document.getElementById("voiceData").value = transcript
+    transcript = result[0].transcript;
+    console.log(transcript);
 
-    console.log('Speech Recognition Result:', transcript);
+    if (transcript.toLowerCase().includes('nebula')) {
+      triggerListeningRobo();
+      transcript = ''
+    }
   };
 
-  recognition.onend = () => {
-    console.log('Speech Recognition Ended');
-    isRecording = false;
-    // sendPrompt(transcript)
-  };
+  // recognition.onend = () => {
+  //   console.log('Speech Recognition Ended');
+  //   isRecording = false;
+  //   // sendPrompt(transcript)
+  // };
 
   recognition.start();
   isRecording = true;
   console.log('Speech Recognition Started');
 };
 
+
 // Stop Recording
 const stopRecording = () => {
+  console.log('stop');
   if (recognition && isRecording) {
     recognition.stop();
     isRecording = false;
     console.log('ended');
     sendPrompt(transcript)
+    document.getElementById("default").style.display = 'block'
+    document.getElementById("bg").style.display = 'none'
+    document.getElementById("second").style.display = 'none'
+    document.getElementById("third").style.display = 'block'
   };
 }
-document.getElementById('startButton').addEventListener('click', startRecording);
-document.getElementById('stopButton').addEventListener('click', stopRecording);
+
+// document.getElementById('startButton').addEventListener('click', startRecording);
+// document.getElementById('stopButton').addEventListener('click', stopRecording);
+
+
+
+
+
+
+
+// const startRecording = () => {
+//   audio.src = ''
+//   document.getElementById("voiceData").value = ''
+//   startTime = Date.now();
+//   console.log(startTime);
+//   // textToSpeech("Hello, this is a test.");
+//   recognition = new webkitSpeechRecognition();
+//   recognition.continuous = true;
+//   recognition.interimResults = true;
+  
+//   recognition.onresult = (event) => {
+//     const result = event.results[event.results.length - 1];
+//     transcript = result[0].transcript
+//     document.getElementById("voiceData").value = transcript
+
+//     console.log('Speech Recognition Result:', transcript);
+//   };
+
+//   recognition.onend = () => {
+//     console.log('Speech Recognition Ended');
+//     isRecording = false;
+//     // sendPrompt(transcript)
+//   };
+
+//   recognition.start();
+//   isRecording = true;
+//   console.log('Speech Recognition Started');
+// };
